52e9428cc6fe4dcfb3b9649ecb538a118b8b55bf
Varvara Folimonova <164979498+var-foli@users.noreply.github.com>
2025-08-03 17:04:51 -0400

    Update prefix.h

--- src/prefix.h
+++ src/prefix.h
@@ -41,7 +41,6 @@ class PrefixTree {
 		void insert(PrefixNode* prefixNode, string name, string title, float completionTime,
 								vector<string> genres, string releaseDate, vector<string> platforms,
 								vector<string> publishers);
-		PrefixNode* retrieve(PrefixNode* prefixNode, string name);
 		tuple<vector<string>, vector<float>, vector<vector<string>>, vector<string>, 
 					vector<vector<string>>, vector<vector<string>>>getAll(PrefixNode* prefixNode);
 		tuple<vector<string>, vector<float>, vector<vector<string>>, vector<string>, 
